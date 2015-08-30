0ea08b98c64b5cfeaaf9b1cbec2fb9e87cb54b3c
Pascal Craponne <pascal@craponne.fr>
2015-08-30 20:09:22 +0200

    Updated dependencies

--- MrAdvice.MVVM/Properties/AssemblyInfo.cs
+++ MrAdvice.MVVM/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Windows.Markup;
 // ReSharper disable once CheckNamespace
 internal static class Product
 {
-    public const string Version = "0.9.6.2";
+    public const string Version = "0.9.6.3";
 }
