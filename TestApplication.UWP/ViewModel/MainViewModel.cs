8d22cd6c831c7220471da3b963786cc1799fec0d
Pascal Craponne <pascal@craponne.fr>
2015-09-17 13:40:09 +0200

    Working view

--- TestApplication.UWP/ViewModel/MainViewModel.cs
+++ TestApplication.UWP/ViewModel/MainViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace TestApplication.UWP.ViewModel
+﻿#region Mr. Advice MVVM
+// Mr. Advice MVVM
+// A simple MVVM package using Mr. Advice aspect weaver
+// https://github.com/ArxOne/MrAdvice.MVVM
+// Released under MIT license http://opensource.org/licenses/mit-license.php
+#endregion
+
+namespace TestApplication.UWP.ViewModel
 {
-    public class MainViewModel
+    using System.ComponentModel;
+    using System.Reflection;
+    using ArxOne.MrAdvice.MVVM.Properties;
+    using ArxOne.MrAdvice.MVVM.ViewModel;
+
+    public class MainViewModel : INotifyPropertyChangedViewModel
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(PropertyInfo propertyInfo, NotifyPropertyChanged sender)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyInfo.Name));
+        }
     }
 }
