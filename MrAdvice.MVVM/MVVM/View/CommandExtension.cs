4ae9451dd7ea5eed8faa5ebc8958ac5eaf297a21
Pascal Craponne <pascal@craponne.fr>
2016-11-27 01:15:18 +0100

    Binding commands to InputBindings

--- MrAdvice.MVVM/MVVM/View/CommandExtension.cs
+++ MrAdvice.MVVM/MVVM/View/CommandExtension.cs
@@ -16,6 +16,7 @@ namespace ArxOne.MrAdvice.MVVM.View
     using System.Windows.Data;
     using System.Windows.Input;
     using System.Windows.Markup;
+    using System.Xaml;
 
     /// <summary>
     /// Allows to bind commands directly to view-model methods
@@ -101,6 +102,33 @@ namespace ArxOne.MrAdvice.MVVM.View
         {
             var provideValueTarget = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
             var targetObject = provideValueTarget.TargetObject;
+
+#if !SILVERLIGHT
+            // InputBinding have a special case
+            var inputBinding = targetObject as InputBinding;
+            if (inputBinding != null)
+            {
+                var rootObjectProvider = (IRootObjectProvider)serviceProvider.GetService(typeof(IRootObjectProvider));
+                var root = (FrameworkElement)rootObjectProvider.RootObject;
+                // once it is bound
+                root.DataContextChanged += delegate
+                {
+                    // find owner
+                    var owner = GetOwner(root, inputBinding);
+                    if (owner == null)
+                        return;
+                    var ownerViewModel = owner.DataContext;
+                    if (ownerViewModel != null)
+                    {
+                        // and set command
+                        var command = GetCommand(ownerViewModel);
+                        inputBinding.Command = command;
+                    }
+                };
+                return null;
+            }
+#endif
+
             var element = targetObject as FrameworkElement;
             if (element == null)
                 return this;
@@ -118,32 +146,17 @@ namespace ArxOne.MrAdvice.MVVM.View
             var targetProperty = provideValueTarget.TargetProperty;
             element.DataContextChanged += delegate
             {
-                var viewModel = element.DataContext;
-                if (viewModel == null)
+                var elementViewModel = element.DataContext;
+                if (elementViewModel == null)
                     return;
 
-                var name = Name;
-                var bindingParameter = name as Binding;
-                // because we bind to a method, this allows us to have a syntax control in XAML editor
-                if (bindingParameter != null)
-                    name = viewModel.GetType().GetMember(bindingParameter.Path.Path).FirstOrDefault();
-
-                var command = new RelayCommand(viewModel, name, _parameterSet ? () => Parameter : (Func<object>)null);
-                command.Command += (sender, e) => Command?.Invoke(sender, e);
-                // TODO: the SetCommand extension method could be much better, especially use directly dependency property
-                // kept here for compatibility, but should be removed
-                if (!element.SetCommand(targetProperty, command, Parameter))
-                {
-                    var targetDependencyProperty = targetProperty as DependencyProperty;
-                    if (targetDependencyProperty != null)
-                        element.SetValue(targetDependencyProperty, command);
-                }
+                var command1 = SetCommand(element, elementViewModel, targetProperty);
 
 #if !SILVERLIGHT
                 // keyboard shortcut
                 if (Key != Key.None)
                 {
-                    var keyBinding = new KeyBinding(command, Key, Modifiers);
+                    var keyBinding = new KeyBinding(command1, Key, Modifiers);
                     var collectingItem = element.FindCollectingItem(ItemCollectionType.KeyBindings);
                     if (collectingItem != null)
                     {
@@ -153,8 +166,62 @@ namespace ArxOne.MrAdvice.MVVM.View
                 }
 #endif
             };
-
             return null;
+        }
+
+#if !SILVERLIGHT
+        /// <summary>
+        /// Gets the owner for given <see cref="InputBinding"/>.
+        /// Currently this method is very poor, and works only for <see cref="Window"/>
+        /// </summary>
+        /// <param name="root">The root.</param>
+        /// <param name="inputBinding">The input binding.</param>
+        /// <returns></returns>
+        private static FrameworkElement GetOwner(DependencyObject root, InputBinding inputBinding)
+        {
+            var descendants = root.GetVisualSelfAndChildren().OfType<FrameworkElement>();
+            var owner = descendants.FirstOrDefault(o => o.InputBindings.Contains(inputBinding));
+            return owner;
+        }
+#endif 
+
+        /// <summary>
+        /// Sets the command to target element and property.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="viewModel">The view model.</param>
+        /// <param name="targetProperty">The target property.</param>
+        /// <returns></returns>
+        private RelayCommand SetCommand(UIElement element, object viewModel, object targetProperty)
+        {
+            var command = GetCommand(viewModel);
+            // TODO: the SetCommand extension method could be much better, especially use directly dependency property
+            // kept here for compatibility, but should be removed
+            if (!element.SetCommand(targetProperty, command, Parameter))
+            {
+                var targetDependencyProperty = targetProperty as DependencyProperty;
+                if (targetDependencyProperty != null)
+                    element.SetValue(targetDependencyProperty, command);
+            }
+            return command;
+        }
+
+        /// <summary>
+        /// Gets the bound command, given the view-model.
+        /// </summary>
+        /// <param name="viewModel">The view model.</param>
+        /// <returns></returns>
+        private RelayCommand GetCommand(object viewModel)
+        {
+            var name = Name;
+            var bindingParameter = name as Binding;
+            // because we bind to a method, this allows us to have a syntax control in XAML editor
+            if (bindingParameter != null)
+                name = viewModel.GetType().GetMember(bindingParameter.Path.Path).FirstOrDefault();
+
+            var command = new RelayCommand(viewModel, name, _parameterSet ? () => Parameter : (Func<object>)null);
+            command.Command += (sender, e) => Command?.Invoke(sender, e);
+            return command;
         }
     }
 #endif
